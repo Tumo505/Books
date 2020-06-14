8e7ab9fd076b048ba2e4bb05ba929725370682c1
Tumo Kgosiyame <kgosiyametumo44@gmail.com>
2020-06-15 01:02:01 +0200

    scroll view

--- app/src/main/java/com/tumo505/books/BookListActivity.java
+++ app/src/main/java/com/tumo505/books/BookListActivity.java
@@ -2,26 +2,70 @@ package com.tumo505.books;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class BookListActivity extends AppCompatActivity {
+    private ProgressBar mLoadingProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
-        TextView tvResult = (TextView) findViewById(R.id.tvResponse);
+        mLoadingProgress = (ProgressBar) findViewById(R.id.pb_loading);
+
         try {
             URL bookUrl = ApiUtil.buildUrl("cooking");
-            String jsonResult = ApiUtil.getJson(bookUrl);
-            tvResult.setText(jsonResult);
+            new BooksQueryTask().execute(bookUrl);
+
         }
         catch (Exception e) {
             Log.d("error", e.getMessage());
+        }
+    }
+
+    public class BooksQueryTask extends AsyncTask<URL,Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchURL = urls[0];
+            String result = null;
+            try {
+                result = ApiUtil.getJson(searchURL);
+            }
+            catch (IOException e) {
+                Log.e("Error", e.getMessage());
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView tvResult = (TextView) findViewById(R.id.tvResponse);
+            TextView tvError = (TextView) findViewById(R.id.tv_error);
+            mLoadingProgress.setVisibility(View.INVISIBLE);
+            if (result == null) {
+                tvResult.setVisibility(View.INVISIBLE);
+                tvError.setVisibility(View.VISIBLE);
+            }
+            else {
+                tvResult.setVisibility(View.VISIBLE);
+                tvError.setVisibility(View.INVISIBLE);
+            }
+            tvResult.setText(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingProgress.setVisibility(View.VISIBLE);
         }
     }
 }
